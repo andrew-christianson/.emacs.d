3a687d7d9fe6dcccdb0fa72237b727ea158dd1b8
andrew.christianson <andrew.christianson>
2014-12-19 15:33:49 -0800

    add haskell and cider packages

--- init.el
+++ init.el
@@ -335,13 +335,6 @@ if point was already at that position, move point to beginning of line."
  ;; If there is more than one, they won't work right.
 
 ;; Theme Settings
-(load-theme 'spacegray t)
-(global-hl-line-mode 1)
-(require 'hlinum)
-(hlinum-activate)
-
-(sml/setup)
-(sml/apply-theme 'respectful)
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -353,7 +346,14 @@ if point was already at that position, move point to beginning of line."
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes (quote ("3a727bdc09a7a141e58925258b6e873c65ccf393b2240c51553098ca93957723" "6a37be365d1d95fad2f4d185e51928c789ef7a4ccf17e7ca13ad63a8bf5b922f" default)))
+ '(custom-safe-themes (quote ("c5a044ba03d43a725bd79700087dea813abcb6beb6be08c7eb3303ed90782482" "6a37be365d1d95fad2f4d185e51928c789ef7a4ccf17e7ca13ad63a8bf5b922f" default)))
  '(display-time-mode t)
  '(show-paren-mode t)
  '(tool-bar-mode nil))
+(load-theme 'spacegray t)
+(global-hl-line-mode 1)
+(require 'hlinum)
+(hlinum-activate)
+
+(sml/setup)
+(sml/apply-theme 'respectful)
