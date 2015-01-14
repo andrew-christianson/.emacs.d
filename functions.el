a23a25a58657fc845c1d25a16154dfe513444751
Andrew Christianson <andrew.christianson@ipsos.com>
2015-01-14 18:13:25 +0000

    Correctly factor and clean init.el

--- functions.el
+++ functions.el
@@ -68,3 +68,12 @@
   (flycheck-mode 1)
   (flycheck-select-checker 'python-pylint)
   )
+(defun smart-beginning-of-line ()
+  "Move point to first non-whitespace character or beginning-of-line.
+Move point to the first non-whitespace character on this line.
+if point was already at that position, move point to beginning of line."
+  (interactive)
+  (let ((oldpos (point)))
+    (back-to-indentation)
+    (and (= oldpos (point))
+         (beginning-of-line))))
--- init.el
+++ init.el
@@ -8,14 +8,12 @@
 (pallet-mode t)
 (package-initialize)
 
-(load-file "details.el")
-(load-file "functions.el")
-
 ;; Visal Basics
 (require 'hlinum)
 (hlinum-activate)
 (global-hl-line-mode 1)
-
+(load-file "~/.emacs.d/details.el")
+(load-file "~/.emacs.d/functions.el")
 ;; Get that cruft outta here
 (menu-bar-mode nil)
 (setq inhibit-startup-screen t)
@@ -51,11 +49,8 @@
 
 ;; Sublime Like Commenting
 
-
-
 (global-set-key (kbd "M-;") 'comment-or-uncomment-region-or-line)
 (global-set-key (kbd "C-M-;") 'recomment-region-or-line)
-
 
 
 ; fetch the list of packages available
@@ -132,60 +127,6 @@
 (global-set-key (kbd "C-S-d") 'kill-whole-line)
 (global-set-key (kbd "M-a") 'align)
 
-;; Sublime Like Commenting
-(defun comment-or-uncomment-region-or-line ()
-    "Comments or uncomments the region or the current line if there's no active region."
-    (interactive)
-    (let (beg end)
-        (if (region-active-p)
-            (setq beg (region-beginning) end (region-end))
-            (setq beg (line-beginning-position) end (line-end-position)))
-        (comment-or-uncomment-region beg end)
-        (next-line)))
-
-(global-set-key (kbd "M-;") 'comment-or-uncomment-region-or-line)
-
-
-(defun smart-beginning-of-line ()
-  "Move point to first non-whitespace character or beginning-of-line.
-Move point to the first non-whitespace character on this line.
-if point was already at that position, move point to beginning of line."
-  (interactive)
-  (let ((oldpos (point)))
-    (back-to-indentation)
-    (and (= oldpos (point))
-         (beginning-of-line))))
-
-(defun split-window-multiple-ways (x y)
-  "Split the current frame into a grid of X columns and Y rows."
-  (interactive "nColumns: \nnRows: ")
-  ;; one window
-  (delete-other-windows)
-  (dotimes (i (1- x))
-      (split-window-horizontally)
-      (dotimes (j (1- y))
-	(split-window-vertically))
-      (other-window y))
-  (dotimes (j (1- y))
-    (split-window-vertically))
-  (balance-windows))
-
-(defun show-buffers-with-major-mode (mode)
-  "Fill all windows of the current frame with buffers using major-mode MODE."
-  (interactive
-   (let* ((modes (loop for buf being the buffers
-		       collect (symbol-name (with-current-buffer buf
-					      major-mode)))))
-     (list (intern (completing-read "Mode: " modes)))))
-  (let ((buffers (loop for buf being the buffers
-		       when (eq mode (with-current-buffer buf
-				       major-mode))
-		       collect buf)))
-    (dolist (win (window-list))
-      (when buffers
-	(show-buffer win (car buffers))
-	(setq buffers (cdr buffers))))))
-
 (global-set-key (kbd "C-c d m g") 'split-window-multiple-ways)
 (global-set-key (kbd "C-c d m t g") 'show-buffers-with-major-mode)
 (global-set-key [home] 'smart-beginning-of-line)
@@ -210,19 +151,300 @@ if point was already at that position, move point to beginning of line."
       ac-show-menu-immediately-on-auto-complete t
       ac-candidate-limit 100
       ac-menu-height 25)
+;; (setq-default ac-sources (append ac-sources '(ac-source-filename ac-source-yasnippet)))
+;; don't wamt this.
+;; Default is:
+;; TAB, C-i 	ac-expand 	Completion by TAB
+;; RET, C-m 	ac-complete 	Completion by RET
+;; down, M-n 	ac-next 	Select next candidate
+;; up, M-p 	ac-previous 	Select previous candidate
+;; C-?, f1 	ac-help 	Show buffer help
+
+;; (define-key ac-completing-map (kbd "TAB") 'ac-complete)
+(define-key ac-completing-map (kbd "C-h") 'ac-quick-help)
+(define-key ac-completing-map (kbd "C-S-c h") 'ac-complete-with-helm)
+(define-key ac-mode-map (kbd "C-S-c c") 'auto-complete)
+
+(global-auto-complete-mode)
+
+;; yasnippet
+(require 'yasnippet)
+(yas-global-mode 1)
+
+;; Company
+;(require 'company)
+;(global-company-mode 0)
+
+;; Ess
+(require 'ess-site)
+(require 'ess-smart-underscore)
+;; (setq inferior-ess-mode-hook
+;;       '(ess-S-mouse-me-menu-commands
+;; 	ess-developer-setup-modeline
+;; 	turn-on-font-lock
+;; 	smartparens-mode))
+(add-hook 'ess-R-post-run-hook 'smartparens-mode)
+(if (eq system-type 'windows-nt)
+    (setq inferior-julia-program-name "~/../julia/Julia-0.3.2/bin/julia")
+)
+
+;; SmartParen
+(require 'smartparens-config)
+(smartparens-global-mode)
+
+
+;; undotree:
+;(require 'undo-tree)
+;(global-undo-tree-mode)
+;; Paredit
+;; (add-hook 'prog-mode-hook 'paredit-everywhere-mode)
+
+;; MultiCursor
+(require 'multiple-cursors)
+(global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
+(global-set-key (kbd "C->") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
+
+;; Linum settings
+;; (require 'linum-mode)
+
+(define-global-minor-mode my-global-linum-mode linum-mode
+  (lambda ()
+    (when (not (memq major-mode
+                     (list 'doc-view-mode 'doc-view-minor-mode)))
+      (linum-mode))))
+
+(my-global-linum-mode 1)
+
+(setq python-shell-interpreter "ipython"
+      python-shell-interpreter-args ""
+      python-shell-prompt-regexp "In \: "
+      python-shell-prompt-output-regexp "Out\: "
+      python-shell-completion-setup-code "from IPython.core.completerlib import module_completion"
+      python-shell-completion-module-string-code "';'.join(module_completion('''%s'''))"
+      python-shell-completion-string-code "';'.join(get_ipython().Completer.all_completions('''%s'''))"
+      python-shell-completion-native-enable (if (eq system-type "windows-nt") nil t)
+      jedi:complete-on-dot t
+      jedi:tooltip-method nil
+      )
+
+(add-hook 'python-mode-hook 'my-jedi-setup)
+;; (add-hook 'python-mode-hook 'my-jedi-settings)
+(add-hook 'python-mode-hook 'my-flycheck-settings)
+	  ;; 'anaconda-mode)
+;; (add-hook 'python-mode-hook '(add-to-list 'company-backends 'company-anaconda))
+;; (add-hook 'python-mode-hook 'ac-anaconda-setup)
+;; (add-hook 'python-mode-hook '(lambda ()
+			       ;; ((company-mode 1)
+				;; (auto-complete-mode -1))))
+
+;; (add-hook 'python-mode-hook
+	  ;; (lambda ()
+	    ;; (flycheck-mode 1)
+	    ;; (flycheck-select-checker 'python-pylint)))
+(eval-after-load 'flycheck
+  '(define-key flycheck-mode-map (kbd "C-c h F") 'helm-flycheck))
+(require 'flycheck-tip)
+(define-key flycheck-mode-map (kbd "C-M-S-n") 'flycheck-tip-cycle)
+(define-key flycheck-mode-map (kbd "C-M-S-p") 'flycheck-tip-cycle-reverse)
+(flycheck-tip-use-timer 'verbose)
+;; (add-to-list 'company-backends 'company-anaconda)
+;; (add-hook 'python-mode-hook 'eldoc-mode)
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
+
+;; (elpy-enable)
+;; (elpy-use-ipython)
+;; (setq elpy-rpc-backend "jedi")
+
+;; OME Grab
+(show-paren-mode 1)
+;; (setq show-paren-style 'expression)
+;;(Global-yascroll-bar-mode 1)
+
+;; Markdown
+(autoload 'markdown-mode "markdown-mode" "Major mode for editing Markdown files" t)
+(add-to-list 'auto-mode-alist '("\\.text\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+
+;; Haskell
+(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
+(add-hook 'haskell-mode-hook 'turn-on-hi2)
+(add-hook 'haskell-mode-hook 'interactive-haskell-mode)
+(eval-after-load 'haskell-mode
+  '(define-key haskell-mode-map (kbd "C-c I ") 'haskell-navigate-imports))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(company-tooltip-minimum-width 40)
- '(custom-safe-themes (quote ("756597b162f1be60a12dbd52bab71d40d6a2845a3e3c2584c6573ee9c332a66e" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "c5a044ba03d43a725bd79700087dea813abcb6beb6be08c7eb3303ed90782482" "6a37be365d1d95fad2f4d185e51928c789ef7a4ccf17e7ca13ad63a8bf5b922f" default))))
+ '(custom-safe-themes (quote ("756597b162f1be60a12dbd52bab71d40d6a2845a3e3c2584c6573ee9c332a66e" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "c5a044ba03d43a725bd79700087dea813abcb6beb6be08c7eb3303ed90782482" "6a37be365d1d95fad2f4d185e51928c789ef7a4ccf17e7ca13ad63a8bf5b922f" default)))
+ '(haskell-process-auto-import-loaded-modules t)
+ '(haskell-process-log t)
+ '(haskell-process-suggest-remove-import-lines t)
+ '(haskell-tags-on-save t))
+
+
+(eval-after-load 'haskell-mode '(progn
+  (define-key haskell-mode-map (kbd "C-c C-l") 'haskell-process-load-or-reload)
+  (define-key haskell-mode-map (kbd "C-c C-z") 'haskell-interactive-switch)
+  (define-key haskell-mode-map (kbd "C-c C-n C-t") 'haskell-process-do-type)
+  (define-key haskell-mode-map (kbd "C-c C-n C-i") 'haskell-process-do-info)
+  (define-key haskell-mode-map (kbd "C-c C-n C-c") 'haskell-process-cabal-build)
+  (define-key haskell-mode-map (kbd "C-c C-n c") 'haskell-process-cabal)
+  (define-key haskell-mode-map (kbd "SPC") 'haskell-mode-contextual-space)))
+(eval-after-load 'haskell-cabal '(progn
+  (define-key haskell-cabal-mode-map (kbd "C-c C-z") 'haskell-interactive-switch)
+  (define-key haskell-cabal-mode-map (kbd "C-c C-k") 'haskell-interactive-mode-clear)
+  (define-key haskell-cabal-mode-map (kbd "C-c C-c") 'haskell-process-cabal-build)
+  (define-key haskell-cabal-mode-map (kbd "C-c c") 'haskell-process-cabal)))
+(eval-after-load 'haskell-mode
+  '(define-key haskell-mode-map (kbd "C-c C-o") 'haskell-compile))
+(eval-after-load 'haskell-cabal
+  '(define-key haskell-cabal-mode-map (kbd "C-c C-o") 'haskell-compile))
+
+(eval-after-load 'haskell-mode
+  '(define-key haskell-mode-map (kbd "C-c C-o") 'haskell-compile))
+(eval-after-load 'haskell-cabal
+  '(define-key haskell-cabal-mode-map (kbd "C-c C-o") 'haskell-compile))
+
+(require 'rainbow-delimiters)
+(rainbow-delimiters-mode)
+;; Javascript
+
+(add-to-list 'auto-mode-alist '("\\.js[x]?\\'" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.json\\'\\|\\.jshintrc\\'" . json-mode))
+
+;; Projectile
+(projectile-global-mode)
+(setq projectile-indexing-method 'alien)
+(setq projectile-enable-caching t)
+;; dirtree
+;; (require 'dirtree)
+
+;; NeoTree
+(require 'sr-speedbar)
+(global-set-key [f8] 'sr-speedbar-toggle)
+;; (setq projectile-switch-project-action 'neotree-projectile-action)
+;; (setq neo--persist-show nil)
+
+;; Magit
+;; Other custom Bindings
+
+;; Backups
+(setq backup-directory-alist `(("." . "~/.saves")))
+(setq backup-by-copying t)
+(setq delete-old-versions t
+  kept-new-versions 6
+  kept-old-versions 2
+  version-control t)
+
+;; Smooth scrolling
+;; due to http://stackoverflow.com/a/4160949/881025
+;; Works well on OSX 10.10
+(require 'smooth-scrolling)
+;; (setq mouse-wheel-follow-mouse 't)
+;; (setq mouse-wheel-scroll-amount '(1 ((shift) . 1)))
+
+;(setq redisplay-dont-pause t
+;  scroll-margin 1
+;  scroll-step 1
+;  scroll-conservatively 10000
+;  scroll-preserve-screen-position 1)
+
+;; (require 'workgroups2)
+;; ;<prefix> <key>
+;; ;<prefix> c    - create workgroup
+;; ;<prefix> A    - rename workgroup
+;; ;<prefix> k    - kill workgroup
+;; ;<prefix> v    - switch to workgroup
+;; ;<prefix> C-s  - save session
+;; ;<prefix> C-f  - load session
+
+;; (setq wg-prefix-key (kbd "C-c z"))
+
+;; ;; Change workgroups session file
+;; (setq wg-session-file "~/.emacs.d/.emacs_workgroups")
+
+;; ;; Set your own keyboard shortcuts to reload/save/switch WGs:
+;; ;; "s" == "Super" or "Win"-key, "S" == Shift, "C" == Control
+;; (global-set-key (kbd "<pause>")     'wg-reload-session)
+;; (global-set-key (kbd "C-S-<pause>") 'wg-save-session)
+;; (global-set-key (kbd "s-z")         'wg-switch-to-workgroup)
+;; (global-set-key (kbd "s-/")         'wg-switch-to-previous-workgroup)
+;; ;; What to do on Emacs exit / workgroups-mode exit?
+;; (setq wg-emacs-exit-save-behavior           'ask)      ; Options: 'save 'ask nil
+;; (setq wg-workgroups-mode-exit-save-behavior 'save)      ; Options: 'save 'ask nil
+
+;; ;; Mode Line changes
+;; ;; Display workgroups in Mode Line?
+;; (setq wg-mode-line-display-on t)          ; Default: (not (featurep 'powerline))
+;; (setq wg-flag-modified t)                 ; Display modified flags as well
+;; (setq wg-mode-line-decor-left-brace "["
+;;       wg-mode-line-decor-right-brace "]"  ; how to surround it
+;;       wg-mode-line-decor-divider ":")
+;; (workgroups-mode 1)
+
+
+
+(if (eq system-type 'window-nt)
+    (setq magit-git-executable "c:/PROGRA~2/Git/bin/git.exe"
+	  git-executable "c:/PROGRA~2/Git/bin/git.exe")
+     )
+
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(require 'git-gutter-fringe)
+
+(global-git-gutter-mode 1)
+(setq-default indicate-buffer-boundaries 'left)
+(setq-default indicate-empty-lines 1)
+
+
+;; Org Mode
+
+(setq org-mobile-directory "~/Dropbox/MobileOrg")
+(global-set-key (kbd "C-c l") 'org-store-link)
+(global-set-key (kbd "C-c c") 'org-capture)
+(global-set-key (kbd "C-c a") 'org-agenda)
+(global-set-key (kbd "C-c b") 'org-iswitchb)
+(require 'org-agenda)
+(setq org-agenda-span 14)
+
+;; see http://www.gnu.org/software/emacs/manual/html_node/elisp/Face-Attributes.html
+;; for details on font settis
+;; weight controls the typeface for source code pro.
+
+(if (or (eq system-type 'windows-nt)
+	(eq system-type 'darwin))
+    (set-face-attribute 'default nil
+			:family "Source Code Pro"
+			:height (if (eq system-type 'windows-nt) 90 110)
+			:weight 'light
+			:width 'normal
+			:foundry 'outline
+			:slant 'normal))
+
+(if (eq system-type 'gnu/linux)
+    (set-face-attribute 'default nil
+			:family "Terminess Powerline"
+			:foundry 'outline
+			:slant 'normal
+			:weight 'light
+			:height 90
+			:width 'normal))
+
+;; Theme Settings
+
+
 
 ;; Themes come after the safe marker
 (load-theme 'solarized-light t)
 (sml/setup)
 (sml/apply-theme 'respectful)
-
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
