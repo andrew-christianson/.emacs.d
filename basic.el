70b2a07f21007cf2e640aa418d68a3a8c1ddc330
Andrew Christianson <andrew.christianson@gmail.com>
2015-03-02 09:32:44 -0800

    actually smooth scrolling

--- basic.el
+++ basic.el
@@ -10,8 +10,8 @@
       kept-old-versions 2
       version-control t
       redisplay-dont-pause t
-      scroll-margin 1
-      scroll-step 1
+      scroll-margin 10
+      scroll-step 0
       scroll-conservatively 10000
       scroll-preserve-screen-position 1
       )
